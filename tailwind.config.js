57a6435cabfc7e5095bffe336e9e0c951fd2e9c3
RAJ DEVKAR <rajdevkar.6@gmail.com>
2021-07-29 15:42:30 +0530

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 module.exports = {
   purge: ["./pages/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        xxs: ".65rem",
+      },
+      animation: {
+        "slow-bounce": "bounce 2s infinite",
+      },
+    },
+    fontFamily: {
+      primary: "'Press Start 2P'",
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
